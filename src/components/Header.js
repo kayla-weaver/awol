841e6493a9d2c757c14ab64e16faacc99cb1c4a6
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-26 11:32:00 -0800

    english and spanish toggle option

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+// import React from "react";
+// import { useTranslation } from "react-i18next";
+// import i18next from "i18next";
+
+// function Header(){
+//   const { t } = useTranslation
+//   // const languages = [
+//   //   {
+//   //     code: 'en',
+//   //     name: 'English',
+//   //     country_code: 'gb'
+//   //   },
+//   //   {
+//   //     code: 'es',
+//   //     name: 'Español',
+//   //     country_code: 'es'
+//   //   }
+//   // ]
+  
+//   // return (
+// //     <React.Fragment>
+// //       <div className="dropdown">
+// //   <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+// //     Change language
+// //   </button>
+// //   <ul className="dropdown-menu">
+// //     {languages.map(({ code, name, country_code}) => (
+// //       <li key={country_code}>
+// //         <button className="dropdown-item" onClick= {() => i18next.changeLanguage(code)}>
+// //           {name}
+// //           </button>
+// //       </li>
+// //     ))}
+    
+// //   </ul>
+// // </div> 
+// // </React.Fragment> 
+// //  );
+// // }
+
+
+// export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {Suspense} from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import App from './components/App';
+
+
+i18n
+  .use(initReactI18next) 
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en','es'],
+    fallbackLng: "en",
+    detection: {
+      order: ['htmlTag','cookie', 'localStorage', 'path', 'subdomain'],
+      cashes: ['cookie']
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  })
+const loadingMarkup= (
+  <div className="py-4 text-center">
+    <h2>Loading...</h2>
+  </div>
+)
+
+ReactDOM.render(
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
