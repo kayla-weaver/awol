21c06a61483dff5c672eea41410fdb190c89069d
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-28 16:26:11 -0800

    restructure language change into header

--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.js'
 import App from './components/App';
 
-
-i18n
-  .use(initReactI18next) 
-  .use(LanguageDetector)
-  .use(HttpApi)
-  .init({
-    supportedLngs: ['en','es'],
-    fallbackLng: "en",
-    detection: {
-      order: ['htmlTag','cookie', 'localStorage', 'path', 'subdomain'],
-      cashes: ['cookie']
-    },
-    backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
-    },
-  })
-const loadingMarkup= (
-  <div className="py-4 text-center">
-    <h2>Loading...</h2>
-  </div>
-)
-
 ReactDOM.render(
-  <Suspense fallback={loadingMarkup}>
+  <Suspense fallback={<div>Loading...</div>}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Suspense>,
   document.getElementById('root')
-)
-
-
+);
